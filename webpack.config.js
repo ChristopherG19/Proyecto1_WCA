1d400b3a396941e762fa8604f14e788c54e9fc43
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2022-05-24 10:49:11 -0600

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-						cacheDirectory: true,
-					},
-				},
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.join(__dirname, './src/index.html'),
-		}),
-	],
-	externals: {
-		React: 'react',
-	},
-	devServer: {
-		static: {
-			directory: path.join(__dirname, 'dist'),
-		},
-		compress: true,
-		port: 9000,
-	},
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'WCA'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            cacheDirectory: true,
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+    }),
+  ],
+  externals: {
+    React: 'react',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'WCA'),
+    },
+    compress: true,
+    port: 9000,
+  },
 }
